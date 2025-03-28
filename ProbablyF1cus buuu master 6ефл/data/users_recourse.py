c3ec344482f59e037c925be2fa33b7427d95a1cb
KachokChort <timajambam@gmail.com>
2025-03-28 16:57:11 +0300

    first

---
+++ ProbablyF1cus buuu master 6ефл/data/users_recourse.py
@@ -0,0 +1,26 @@
+from flask_restful import
+
+
+
+
+
+def abort_if_news_not_found(news_id):
+    session = db_session.create_session()
+    news = session.query(News).get(news_id)
+    if not news:
+        abort(404, message=f"News {news_id} not found")
+
+class UsersResource(Resource):
+    def get(self, news_id):
+        abort_if_news_not_found(news_id)
+        session = db_session.create_session()
+        news = session.query(News).get(news_id)
+        return jsonify({'news': news.to_dict(
+            only=('title', 'content', 'user_id', 'is_private'))})
+    def delete(self, news_id):
+        abort_if_news_not_found(news_id)
+        session = db_session.create_session()
+        news = session.query(News).get(news_id)
+        session.delete(news)
+        session.commit()
+        return jsonify({'success': 'OK'})
